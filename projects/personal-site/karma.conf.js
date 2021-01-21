97bfacb5e204a0969919a0fe16847ccc0eb5c17e
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-01-21 22:53:38 +1300

    Pipeline fix with chromium

--- projects/personal-site/karma.conf.js
+++ projects/personal-site/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
+    browsers: ['Chrome', 'ChromiumHeadless'],
     singleRun: false,
     restartOnFileChange: true,
   });
