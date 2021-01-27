887b376bc52cdaf98ebba18886cea3001c2cc484
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-01-27 21:57:42 +1300

    created components module

--- projects/a-lib/src/public-api.ts
+++ projects/a-lib/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of a-lib
  */
 
-export * from './lib/a-lib.service';
-export * from './lib/a-lib.component';
-export * from './lib/a-lib.module';
+export * from './components/components.module';
