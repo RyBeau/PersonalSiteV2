1e51bbea4a7e4911fcc1c0b7ee13c955f86c3d40
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-01-27 21:57:23 +1300

    deleted default component

--- projects/a-lib/src/lib/a-lib.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ALibComponent } from './a-lib.component';
-
-@NgModule({
-  declarations: [ALibComponent],
-  imports: [],
-  exports: [ALibComponent],
-})
-export class ALibModule {}
