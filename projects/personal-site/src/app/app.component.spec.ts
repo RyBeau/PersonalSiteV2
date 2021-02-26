a1fbe86ce60961b49c240d2e61015f64f5412c08
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-02-26 21:17:54 +1300

    fix unit test

--- projects/personal-site/src/app/app.component.spec.ts
+++ projects/personal-site/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('personal-site');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('personal-site app is running!');
-  });
 });
